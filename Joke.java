088fd00b2692c87f18614f792790090e74a0f113
PengfeiCui <perfeyCui@126.com>
2019-07-18 22:26:27 +0800

    Update and rename hellogithub.java to Joke.java

---
+++ Joke.java
@@ -0,0 +1,7 @@
+public class hellogithub
+{
+   public static void main(String args[])
+   {
+     System.out.println("Hello perfey, How to program your life!\n Have u decided? \n");
+   }
+}
--- hellogithub.java
+++
@@ -1,7 +0,0 @@
-public class hellogithub
-{
-   public static void main(String args[])
-   {
-     System.out.println("Hello world again for perfey!");
-   }
-}
